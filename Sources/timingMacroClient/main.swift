9237f38f8a3b5af19ea21f2a8d227dc517c3bcdc
Rachit <rdprajapati@icloud.com>
2024-07-13 12:40:32 -0400

    Added usage code for better preview

--- Sources/timingMacroClient/main.swift
+++ Sources/timingMacroClient/main.swift
@@ -1,9 +1,13 @@
 import timingMacro
 import Foundation 
     
+// This is a file to see the usage of the macro and the generated class methods. 
     
 #timify({
     print("hello world")
 })
 
-
+//Usage
+CodePerformanceAnalyzer.logExecutionTime(withDescription: "This is a log message")
+let rec_time_avg = CodePerformanceAnalyzer.measureAverageTime(repeatCount: 3)
+debugPrint(rec_time_avg)
